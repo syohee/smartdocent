fcd43130675e9eeee905b03586ba186b8dff00f3
joongookang <joongoo23@gmail.com>
2018-04-03 08:55:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tg/myapplication/LocationLookup.java
@@ -0,0 +1,47 @@
+package com.example.tg.myapplication;
+
+import android.graphics.Color;
+import android.location.Location;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+
+/**
+ * Created by TG on 2018-04-02.
+ */
+
+public class LocationLookup {
+    String TAG_JSON="webnautes";
+
+    double distance;
+    public int lookup(Location location, String mJsonString){
+        try {
+            Location locationA = new Location("point A");
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject ddObject = jsonArray.getJSONObject(i);
+                String lat = ddObject.getString("latitude");
+                String lon = ddObject.getString("longitude");
+                locationA.setLatitude(Double.parseDouble(lat));
+                locationA.setLongitude(Double.parseDouble(lon));
+                distance = locationA.distanceTo(location);
+                if(distance <= 20.0005){
+                    return i+1;
+                }
+            }
+            return 0;
+        }catch (Exception e){
+            e.printStackTrace();
+            return 404;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tg/myapplication/MapsActivity.java
@@ -0,0 +1,257 @@
+package com.example.tg.myapplication;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.MediaPlayer;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.os.AsyncTask;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap Current, mMap;
+    private static ArrayList<HashMap<String, String>> mArrayList = new ArrayList<>();
+    public String first_cultural_code = "1";
+    public static  int ste = 0;
+    private Button b1, b2, b3;
+    private SeekBar seekbar;
+    private TextView tx1, tx2;
+    private double startTime = 0;
+    private double finalTime = 0;
+    private int forwardTime = 5000;
+    public static int oneTimeOnly = 0;
+    private  static MediaPlayer mediaPlayer = new MediaPlayer();
+
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        b2 = (Button) findViewById(R.id.button2);
+        b3 = (Button) findViewById(R.id.button3);
+        seekbar = (SeekBar) findViewById(R.id.seekBar);
+        seekbar.setClickable(false);
+        b3.setEnabled(false);
+        tx1 = (TextView) findViewById(R.id.textView2);
+        tx2 = (TextView) findViewById(R.id.textView3);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        GpsPermissionCheckForMashMallo();
+        Log.d("Main", "onCreate");
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        Log.d("Main", "isGPSEnabled=" + isGPSEnabled);
+        Log.d("Main", "isNetworkEnabled=" + isNetworkEnabled);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+
+                finalTime = mediaPlayer.getDuration();
+                startTime = mediaPlayer.getCurrentPosition();
+
+                if (oneTimeOnly == 0) {
+                    seekbar.setMax((int) finalTime);
+                    oneTimeOnly = 1;
+                }
+                tx2.setText(String.format("%d min, %d sec",
+                        TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
+                        TimeUnit.MILLISECONDS.toSeconds((long) finalTime) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long) finalTime)))
+                );
+
+                tx1.setText(String.format("%d min, %d sec",
+                        TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                        TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long) startTime)))
+                );
+
+                seekbar.setProgress((int) startTime);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+//                b2.setEnabled(false);
+//                b3.setEnabled(true);
+            }
+        });
+
+    }
+
+
+
+
+    public void onMapReady(final GoogleMap googleMap) {
+        Current = googleMap;
+        mMap = googleMap;
+//        // Add a marker in Sydney and move the camera
+        googleMap.getUiSettings().setScrollGesturesEnabled(false);
+        googleMap.getUiSettings().setZoomGesturesEnabled(false);
+        final LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                double lat = location.getLatitude();
+                double lng = location.getLongitude();
+                LatLng sydney = new LatLng(lat, lng);
+                test server = new test();
+                Jsonclear clear = new Jsonclear();
+                String test = server.test(first_cultural_code);
+                mArrayList = clear.showResult(test);
+                Current.addMarker(new MarkerOptions().position(sydney).title("내위치"));
+                Current.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                Current.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney,18));
+                drawMarker(location,test);
+                HttpURLConnection test1 = new HttpURLConnection();
+                LocationLookup asd = new LocationLookup();
+                int nare = asd.lookup(location,test);
+                if(nare >=1) {
+                    mediaPlayer = test1.StartDoecnt("http://35.184.38.112/" + nare + ".mp3");
+                    b3.setEnabled(true);
+                }
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mp) {
+                        //여기다 완료 후 코드 작성
+                        b3.setEnabled(false);
+                    }
+                });
+
+            }
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        // 수동으로 위치 구하기
+        String locationProvider = LocationManager.GPS_PROVIDER;
+        Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+        if (lastKnownLocation != null) {
+            double lng = lastKnownLocation.getLatitude();
+            double lat = lastKnownLocation.getLatitude();
+
+
+            Log.d("Main", "longtitude=" + lng + ", latitude=" + lat);
+        }
+}
+
+    // 마크 생성
+    private void drawMarker(Location location, String mJsonString){
+     String TAG_JSON="webnautes";
+        try {
+            Current.clear();
+            LatLng currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
+            Current.addMarker(new MarkerOptions().position(currentPosition).title("내위치"));
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject ddObject = jsonArray.getJSONObject(i);
+                String lat = ddObject.getString("latitude");
+                String lon = ddObject.getString("longitude");
+                String name = ddObject.getString("second_cultural_code");
+                LatLng Heritage = new LatLng(Double.parseDouble(lat), Double.parseDouble(lon));
+                mMap.addMarker(new MarkerOptions().position(Heritage).title(name));
+                Current.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
+                Current.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, 18));
+//        logView.setText("latitude: " + location.getLatitude() + ", longitude: " + location.getLongitude());
+                CircleOptions circle1KM = new CircleOptions().center(Heritage) //원점
+                        .radius(10)      //반지름 단위 : m
+                        .strokeWidth(0f)  //선너비 0f : 선없음
+                        .fillColor(Color.parseColor("#880000ff"));
+                mMap.addCircle(circle1KM);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
+    }
+
+
+
+    //권한 확인
+    public void GpsPermissionCheckForMashMallo() {
+
+        //마시멜로우 버전 이하면 if문에 걸리지 않습니다.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("GPS 사용 허가 요청");
+            alertDialog.setMessage("앰버요청 발견을 알리기위해서는 사용자의 GPS 허가가 필요합니다.\n('허가'를 누르면 GPS 허가 요청창이 뜹니다.)");
+            // OK 를 누르게 되면 설정창으로 이동합니다.
+            alertDialog.setPositiveButton("허가",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(com.example.tg.myapplication.MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+
+                        }
+                    });
+            // Cancle 하면 종료 합니다.
+            alertDialog.setNegativeButton("거절",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+            alertDialog.show();
+        }
+    }
+}
